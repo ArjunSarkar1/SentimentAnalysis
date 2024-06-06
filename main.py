35d444f42834f772c6a146fafae9bf592c2c2c48
arjunsarkar1 <arjunsarkar82@gmail.com>
2024-06-06 10:49:54 -0500

    parsed data for 'AMZN' as demo; need to clean it up

--- main.py
+++ main.py
@@ -15,12 +15,53 @@ for ticker in ticker_symbols:
     response = urlopen(req)
     
     html_output = BeautifulSoup(response, features='html.parser')
-    news_table = html_output.find(id="news-table")
-    news_tables[ticker] = news_tables
+    tables = html_output.find(id="news-table")
+    news_tables[ticker] = tables
 
     # print(html_output)
     # print(response)
     break
 
-print(news_tables['AMZN'])
+"""
+Parsed Data List consists of:
+- Ticker Symbol
+- Date
+- Time
+- News HeadLine 
+"""
+all_parsed_data = []
+all_parsed_data_links = []
+
+for ticker, news in news_tables.items():
+
+    for rows in tables.findAll('tr'):
+        headlines = rows.a.text.strip()
+        date_time = rows.td.text.strip().split(' ')
+        headline_link = rows.findAll('td')[1].a['href']
+
+        if len(date_time) > 1:
+            date = date_time[0]
+            time = date_time[1]
+        else:
+            time = date_time[0]
+
+        all_parsed_data.append([ticker, date, time, headlines])
+        all_parsed_data_links.append([headline_link])
+
+# print(all_parsed_data)
+print(all_parsed_data_links)
+    
+    # To Get the Link of Headline
+    # print(row.findAll('td')[1].a['href'])
+    
+
+
+
+# for row in amzn_data.findAll('tr'):
+#     print(row.td.text.strip() + " -> " + row.a.text.strip())    
+#     print("-" * 80)
+# print(f"Link: {link}")
+# print(f"Date & Time: {date_time}")
+# print(f"Headline: {headlines}")
+# print(f"Link: {link}")
 
